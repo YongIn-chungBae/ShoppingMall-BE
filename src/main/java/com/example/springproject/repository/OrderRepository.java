fdb133167c4725bce7d473ed98356ed6758be9ba
cndqjacndqja <cndqjacndqja@gmail.com>
2021-06-14 17:12:49 +0900

    Fix [POST] 제품 주문하기 수정 - 수량 뺴주기

--- src/main/java/com/example/springproject/repository/OrderRepository.java
+++ src/main/java/com/example/springproject/repository/OrderRepository.java
@@ -10,7 +10,4 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("select u from User u JOIN Order o ON u.idx = o.userIdx")
     List<User> findUser();
-
-//    @Query("select o.idx, o.state, o.count, p.idx, p.name, p.image, p.price from Order o JOIN Product p ON o.productIdx = p.idx WHERE u.idx = ?idx")
-//    List<OrderListDTO> orderList(@Param("idx") int idx);
 }
